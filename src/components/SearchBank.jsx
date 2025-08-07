49b123ebf17ec0ad4c3d5df2054ea2c116732266
tw30006 <original.heart.2016@gmail.com>
2025-08-07 21:41:20 +0800

    feat: 點擊銀行名稱的li渲染在input上

--- src/components/SearchBank.jsx
+++ src/components/SearchBank.jsx
@@ -1,23 +1,43 @@
-const searchBank = () => {
+import { useState, useEffect } from "react";
+
+const searchBank = ({ headOfficeList }) => {
+  console.log(headOfficeList);
+  const [selectedHeadOffice, setSelectedHeadOffice] = useState("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleSelectOption = (headOffice) => {
+    setSelectedHeadOffice(`${headOffice.code} ${headOffice.bankName}`);
+  };
+
   return (
     <>
       <div className="flex justify-center my-3">
         <section className="mx-2">
-          <label>銀行名稱</label>
+          <label htmlFor="bankName">銀行名稱</label>
           <div className="relative w-[16rem]">
             <div className="flex w-full items-center justify-between rounded bg-white p-2 ring-1 ring-gray-300">
               <input
-                v-model="search"
                 className="w-full outline-none"
                 placeholder="請輸入關鍵字或銀行代碼..."
+                id="bankName"
+                value={selectedHeadOffice}
+                onChange={(e) => setSelectedHeadOffice(e.target.value)}
               />
             </div>
             <ul className="z-2 absolute mt-1 w-full rounded bg-gray-50 ring-1 ring-gray-300 max-h-60 overflow-y-auto">
               <li className="w-full rounded bg-gray-50 text-center py-2"></li>
-              <li
-                v-for="headOffice in filteredHeadOffices"
-                className="cursor-pointer p-2 hover:bg-gray-200"
-              ></li>
+              {headOfficeList.map((headOffice) => {
+                return (
+                  <li
+                    className="cursor-pointer p-2 hover:bg-gray-200"
+                    key={headOffice.code}
+                    onClick={() => handleSelectOption(headOffice)}
+                  >
+                    <span className="mr-2">{headOffice.code}</span>
+                    {headOffice.bankName}
+                  </li>
+                );
+              })}
             </ul>
           </div>
           <div className="text-gray-500 text-sm">
@@ -25,15 +45,13 @@ const searchBank = () => {
           </div>
         </section>
         <section>
-          <label>分行名稱</label>
+          <label htmlFor="branchName">分行名稱</label>
           <div className="relative w-[10rem]">
-            <button className="flex w-full items-center justify-between rounded bg-white p-2 ring-1 ring-gray-300">
-              <span></span>
-            </button>
-            <ul
-              v-if="openBranch"
-              className="z-2 absolute mt-1 w-full rounded bg-gray-50 ring-1 ring-gray-300 max-h-60 overflow-y-auto"
-            >
+            <button
+              className="flex w-full items-center justify-between rounded bg-white p-2 ring-1 ring-gray-300"
+              id="branchName"
+            ></button>
+            <ul className="z-2 absolute mt-1 w-full rounded bg-gray-50 ring-1 ring-gray-300 max-h-60 overflow-y-auto">
               <li className="cursor-pointer p-2 hover:bg-gray-200"></li>
             </ul>
           </div>
