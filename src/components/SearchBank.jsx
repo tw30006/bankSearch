a67c05d3314691ffcac6d4d6711b93b1935ff39e
tw30006 <original.heart.2016@gmail.com>
2025-08-10 22:26:16 +0800

    feat: 銀行名稱下拉選擇＆關鍵字搜尋的功能

--- src/components/SearchBank.jsx
+++ src/components/SearchBank.jsx
@@ -3,12 +3,34 @@ import { useState, useEffect } from "react";
 const searchBank = ({ headOfficeList }) => {
   console.log(headOfficeList);
   const [selectedHeadOffice, setSelectedHeadOffice] = useState("");
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isOpenHeadOffice, setIsOpenHeadOffice] = useState(false);
+  const [filterHeadOffice, setFilterHeadOffice] = useState([]);
 
   const handleSelectOption = (headOffice) => {
     setSelectedHeadOffice(`${headOffice.code} ${headOffice.bankName}`);
+    setIsOpenHeadOffice(false);
+  };
+  useEffect(() => {
+    setFilterHeadOffice(headOfficeList);
+  }, [headOfficeList]);
+  const handleHeadOfficeList = () => {
+    setIsOpenHeadOffice(!isOpenHeadOffice);
   };
 
+  const handleInputHeadOffice = (e) => {
+    const keyword = e.target.value;
+    const normalize = keyword.trim().toLowerCase();
+
+    setSelectedHeadOffice(keyword);
+    const next = headOfficeList.filter((headOffice) => {
+      return (
+        headOffice.bankName.includes(normalize) ||
+        headOffice.code.includes(normalize)
+      );
+    });
+    setFilterHeadOffice(next);
+    setIsOpenHeadOffice(true);
+  };
   return (
     <>
       <div className="flex justify-center my-3">
@@ -21,24 +43,33 @@ const searchBank = ({ headOfficeList }) => {
                 placeholder="請輸入關鍵字或銀行代碼..."
                 id="bankName"
                 value={selectedHeadOffice}
-                onChange={(e) => setSelectedHeadOffice(e.target.value)}
+                onChange={(e) => handleInputHeadOffice(e)}
+                onFocus={() => setIsOpenHeadOffice(true)}
               />
+              <span
+                className="material-symbols-outlined"
+                onClick={() => handleHeadOfficeList()}
+              >
+                {isOpenHeadOffice ? "stat_1" : "stat_minus_1"}
+              </span>
             </div>
-            <ul className="z-2 absolute mt-1 w-full rounded bg-gray-50 ring-1 ring-gray-300 max-h-60 overflow-y-auto">
-              <li className="w-full rounded bg-gray-50 text-center py-2"></li>
-              {headOfficeList.map((headOffice) => {
-                return (
-                  <li
-                    className="cursor-pointer p-2 hover:bg-gray-200"
-                    key={headOffice.code}
-                    onClick={() => handleSelectOption(headOffice)}
-                  >
-                    <span className="mr-2">{headOffice.code}</span>
-                    {headOffice.bankName}
-                  </li>
-                );
-              })}
-            </ul>
+            {isOpenHeadOffice && (
+              <ul className="z-2 absolute mt-1 w-full rounded bg-gray-50 ring-1 ring-gray-300 max-h-60 overflow-y-auto">
+                <li className="w-full rounded bg-gray-50 text-center py-2"></li>
+                {filterHeadOffice.map((headOffice) => {
+                  return (
+                    <li
+                      className="cursor-pointer p-2 hover:bg-gray-200"
+                      key={headOffice.code}
+                      onClick={() => handleSelectOption(headOffice)}
+                    >
+                      <span className="mr-2">{headOffice.code}</span>
+                      {headOffice.bankName}
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
           </div>
           <div className="text-gray-500 text-sm">
             可使用下拉選單或直接輸入關鍵字查詢
